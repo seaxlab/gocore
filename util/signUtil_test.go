247ebf26653c3a4287e56133439a24bec84d23df
shi.pengyan <shipengyan@yuantutech.com>
2020-01-21 13:25:32 +0800

    go mod

---
+++ util/signUtil_test.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"testing"
+	"time"
+)
+
+// spy 2020/1/21
+
+func TestSignUtil(t *testing.T) {
+
+	params := map[string]interface{}{}
+	params["method"] = "get"
+	params["app_key"] = "abc123456"
+	params["format"] = "json"
+	params["v"] = "1.0"
+	params["sign_method"] = "md5"
+	params["timestamp"] = time.Now().Format("2006-01-02 15:04:05")
+
+	appSecret := "abcexvsh12"
+
+	logger.Info("file size ", GetSign(appSecret, params))
+}
---
+++ util/test/test_test.go
@@ -0,0 +1,11 @@
+package test
+
+// spy 2020/1/21
+
+import (
+	"../../log"
+	"os"
+)
+
+// Logger
+var logger = log.NewLogger(os.Stdout)
