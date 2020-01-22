28950556fae9ee7c9190d288a6647342abb93749
shi.pengyan <shipengyan@yuantutech.com>
2020-01-22 17:27:40 +0800

    template

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a
 	github.com/metakeule/fmtdate v1.1.2
 	github.com/stretchr/testify v1.4.0
+	github.com/valyala/fasttemplate v1.1.0
 )
---
+++ template/templateUtil_test.go
@@ -0,0 +1,23 @@
+package template
+
+import (
+	"gitee.com/shi.pengyan/go-core/log"
+	"os"
+	"testing"
+)
+
+// spy 2020/1/22
+var logger = log.NewLogger(os.Stdout)
+
+func TestTemplate(t *testing.T) {
+	templateStr := "http://{host}/?q={query}&foo={bar}{bar}"
+
+	varMap := map[string]interface{}{
+		"host":  "www.baidu.com",
+		"query": "helloworld",
+		"bar":   "789",
+	}
+
+	content := Format(templateStr, varMap)
+	logger.Info(content)
+}
