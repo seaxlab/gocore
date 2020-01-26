992425396ef05e07a50daf5f5bc47732b83a412a
shi.pengyan <shipengyan@yuantutech.com>
2020-01-26 17:16:15 +0800

    reflection

---
+++ reflection/invoke.go
@@ -0,0 +1,13 @@
+package reflection
+
+import "reflect"
+
+// spy 2020/1/26
+
+func Invoke(any interface{}, name string, args ...interface{}) {
+	inputs := make([]reflect.Value, len(args))
+	for i, _ := range args {
+		inputs[i] = reflect.ValueOf(args[i])
+	}
+	reflect.ValueOf(any).MethodByName(name).Call(inputs)
+}
---
+++ reflection/reflection.go
@@ -0,0 +1,11 @@
+package reflection
+
+import (
+	"gitee.com/shi.pengyan/go-core/log"
+	"os"
+)
+
+// spy 2020/1/26
+
+// Logger
+var logger = log.NewLogger(os.Stdout)
