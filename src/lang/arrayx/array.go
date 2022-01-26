6fe0c30659de52647b9ef8ca6660e23954041d54
shipengyan <shipengyan@yuantutech.com>
2022-01-26 08:49:14 +0800

    array clean

--- src/lang/arrayx/array.go
+++ src/lang/arrayx/array.go
@@ -1,6 +1,9 @@
 package arrayx
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 // spy 2020/5/1
 
@@ -8,9 +11,147 @@ func ToStrArray(data interface{}) []string {
 	return data.([]string)
 }
 
-func Shuffle(data []int) {
-	for i := range data {
-		j := rand.Intn(i + 1)
-		data[i], data[j] = data[j], data[i]
+func Shuffle(data []interface{}) {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(data), func(i, j int) { data[i], data[j] = data[j], data[i] })
+}
+
+func ShuffleNew(data []interface{}) []interface{} {
+	// clone first
+	result := make([]interface{}, 0, len(data))
+	for _, v := range data {
+		result = append(result, v)
 	}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(result), func(i, j int) { result[i], result[j] = result[j], result[i] })
+	return result
+}
+
+// ShuffleInt Int
+func ShuffleInt(data []int) {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(data), func(i, j int) { data[i], data[j] = data[j], data[i] })
+}
+
+// ShuffleIntNew an array
+func ShuffleIntNew(data []int) []int {
+	// clone first
+	result := make([]int, 0, len(data))
+	for _, v := range data {
+		result = append(result, v)
+	}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(result), func(i, j int) { result[i], result[j] = result[j], result[i] })
+	return result
+}
+
+type MapFunc func(interface{}) interface{}
+
+// Map 经过MapFunc算子进行计算
+// Invokes `MapFunc` for each element in the array. Creates a new array containing the values returned by `MapFunc`
+// Example:
+//	func MyMapFunc(v interface{}) interface{} {
+//		return v.(int) * 3
+//	}
+//	var myArray = []interface{}{1, 2, 3, 4}
+//	result := arrayx.Map(myArray, MyMapFunc)  // [3, 6, 9, 12]
+func Map(arr []interface{}, mapFunc MapFunc) []interface{} {
+	if arr == nil || mapFunc == nil {
+		return arr
+	}
+
+	result := make([]interface{}, 0, len(arr))
+	for _, v := range arr {
+		result = append(result, mapFunc(v))
+	}
+	return result
+}
+
+// Compact 去除nil元素
+func Compact(arr []interface{}) []interface{} {
+	if arr == nil {
+		return arr
+	}
+
+	result := make([]interface{}, 0, len(arr))
+	for _, v := range arr {
+		if v != nil {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+//
+func Flatten(arr []interface{}) []interface{} {
+	if arr == nil {
+		return arr
+	}
+
+	result := make([]interface{}, 0, len(arr))
+	for _, v := range arr {
+		switch v.(type) {
+		case []interface{}:
+			fl := Flatten(v.([]interface{}))
+			for _, f := range fl {
+				result = append(result, f)
+			}
+		default:
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+// Merge 合并两个数组
+func Merge(a, b []interface{}) []interface{} {
+	return append(a, b...)
+}
+
+func MergeInt(slice1, slice2 []int) (c []int) {
+	c = append(slice1, slice2...)
+	return
+}
+
+func MergeInt64(slice1, slice2 []int64) (c []int64) {
+	c = append(slice1, slice2...)
+	return
+}
+
+func MergeString(slice1, slice2 []string) (c []string) {
+	c = append(slice1, slice2...)
+	return
+}
+
+// Unique 去重
+// Returns a new array with duplicates removed.
+// Example:
+//	var myArray = []interface{}{1, 2, 3, 3, 4}
+//	result := Unique(myArray)  // [1, 2, 3, 4]
+func Unique(arr []interface{}) []interface{} {
+	if arr == nil || len(arr) <= 1 {
+		return arr
+	}
+
+	result := make([]interface{}, 0, len(arr))
+	for _, v := range arr {
+		if len(result) == 0 {
+			result = append(result, v)
+			continue
+		}
+
+		duplicate := false
+		for _, r := range result {
+			if r == v {
+				duplicate = true
+				break
+			}
+		}
+
+		if !duplicate {
+			result = append(result, v)
+		}
+	}
+	return result
 }
--- src/lang/arrayx/array_test.go
+++ src/lang/arrayx/array_test.go
@@ -2,6 +2,8 @@ package arrayx
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 )
 
@@ -25,8 +27,119 @@ func TestAppend(t *testing.T) {
 }
 
 func TestShuffle(t *testing.T) {
-	data := []int{5, 4, 6, 7, 9, 10}
+	data := []interface{}{"first", "second", "third", "111", "23"}
 
 	Shuffle(data)
 	fmt.Println(data)
+}
+
+func TestShuffleInt(t *testing.T) {
+	data := []int{5, 4, 6, 7, 9, 10}
+
+	ShuffleInt(data)
+	fmt.Println(data)
+}
+
+func TestShuffleIntNew(t *testing.T) {
+	data := []int{5, 4, 6, 7, 9, 10}
+	newData := ShuffleIntNew(data)
+	fmt.Println(data)
+	fmt.Println(newData)
+}
+
+func MyMapFunc(v interface{}) interface{} {
+	return v.(int) * 3
+}
+
+func TestMap(t *testing.T) {
+
+	var myArray = []interface{}{1, 2, 3, 4}
+	result := Map(myArray, MyMapFunc) // [3, 6, 9, 12]
+
+	fmt.Println(result)
+}
+
+func TestCompact(t *testing.T) {
+	var arr = []interface{}{1, 2, 3, 4, nil, 5, "hello world", nil}
+
+	result := Compact(arr)
+	assert.Equal(t, 6, len(result))
+	assert.Equal(t, 1, result[0].(int))
+	assert.Equal(t, 2, result[1].(int))
+	assert.Equal(t, 3, result[2].(int))
+	assert.Equal(t, 4, result[3].(int))
+	assert.Equal(t, 5, result[4].(int))
+	assert.Equal(t, "hello world", result[5].(string))
+}
+
+func TestFlatten(t *testing.T) {
+	arr1 := []interface{}{1, 2, 3, 4}    // [1, 2, 3, 4]
+	arr2 := []interface{}{5, 6, 7, arr1} // [5, 6, 7, [1, 2, 3, 4]]
+	result := Flatten(arr2)              // [5, 6, 7, 1, 2, 3, 4]
+
+	log.Println(result)
+}
+
+func TestMerge(t *testing.T) {
+	x := []int{1, 2, 3}
+	y := []int{4, 5, 6}
+
+	z := append(x, y...)
+	fmt.Println(z)
+}
+
+// 还不如上面的简洁
+func TestMerge2(t *testing.T) {
+	x := []int{1, 2, 3}
+	y := []int{4, 5, 6}
+
+	// []T to an []interface{}
+	// https://go.dev/doc/faq#convert_slice_of_interface
+	x2 := make([]interface{}, len(x))
+	for i, v := range x {
+		x2[i] = v
+	}
+
+	y2 := make([]interface{}, len(y))
+	for i, v := range y {
+		y2[i] = v
+	}
+
+	a := Merge(x2, y2)
+	fmt.Println(a)
+}
+
+func TestMergeString(t *testing.T) {
+	x := []string{"a", "b"}
+	y := []string{"c"}
+
+	result := MergeString(x, y)
+	fmt.Println(result)
+}
+
+func TestUnique(t *testing.T) {
+	var myArray = []interface{}{1, 2, 3, 3}
+	result := Unique(myArray)
+	assert.Equal(t, 3, len(result))
+	assert.Equal(t, 1, result[0].(int))
+	assert.Equal(t, 2, result[1].(int))
+	assert.Equal(t, 3, result[2].(int))
+
+	var myArray2 = []interface{}{"something", 1, 1.0, "you know what", 1, "something"}
+	result = Unique(myArray2)
+	assert.Equal(t, 4, len(result))
+	assert.Equal(t, "something", result[0].(string))
+	assert.Equal(t, 1, result[1].(int))
+	assert.Equal(t, 1.0, result[2].(float64))
+	assert.Equal(t, "you know what", result[3].(string))
+
+	assert.Nil(t, Unique(nil))
+
+	var myArray3 = []interface{}{"lone"}
+	assert.Equal(t, myArray3, Unique([]interface{}(myArray3)))
+
+	var myArray4 = []interface{}{"lone", "lone"}
+	result = Unique(myArray4)
+	assert.Equal(t, 1, len(result))
+	assert.Equal(t, "lone", myArray4[0].(string))
 }
